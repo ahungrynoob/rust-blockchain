b32ce6fd43d2e96000401ff2e8bd07eac9575be8
ahungrynoob <dxd_sjtu@outlook.com>
2021-06-20 14:57:00 +0800

    test: utils

---
+++ utils/src/coder.rs
@@ -0,0 +1,38 @@
+use bincode;
+use serde::{Deserialize, Serialize};
+
+pub fn my_serialize<T: ?Sized>(value: &T) -> Vec<u8>
+where
+    T: Serialize,
+{
+    let serialized = bincode::serialize(value).unwrap();
+    serialized
+}
+
+pub fn my_deserialize<'a, T>(bytes: &'a [u8]) -> T
+where
+    T: Deserialize<'a>,
+{
+    let deserialized = bincode::deserialize(bytes).unwrap();
+    deserialized
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+#[cfg(test)]
+mod tests {
+    use crate::coder::Point;
+    use crate::coder::{my_deserialize, my_serialize};
+
+    #[test]
+    fn coder_works() {
+        let point = Point { x: 1, y: 1 };
+        let se = my_serialize(&point);
+        let de: Point = my_deserialize(&se);
+
+        assert_eq!(de, point);
+    }
+}
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,18 +1,1 @@
-use bincode;
-use serde::{Deserialize, Serialize};
-
-pub fn my_serialize<T: ?Sized>(value: &T) -> Vec<u8>
-where
-    T: Serialize,
-{
-    let serialized = bincode::serialize(value).unwrap();
-    serialized
-}
-
-pub fn my_deserialize<'a, T>(bytes: &'a [u8]) -> T
-where
-    T: Deserialize<'a>,
-{
-    let deserialized = bincode::deserialize(bytes).unwrap();
-    deserialized
-}
+pub mod coder;
